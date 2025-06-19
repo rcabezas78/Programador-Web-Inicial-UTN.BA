3774b188af2ba9b94b36480d7b3bb63629a4055a
Rodrigo Cabezas <rcabezas@umaza.edu.ar>
2025-06-19 12:38:23 -0300

    M3U3 correo

--- M3U3/routes/index.js
+++ M3U3/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+/*declaramos la libreria nodemailer que vamos a utilizar*/
+var nodemailer=require('nodemailer')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  // res.render('index', { title: 'M3U3 Express' });
+  res.render('index');
 });
+
+// router.post('/',async(req,res,next)=>{
+//   var nombre=req.body.nombre;
+//   var apellido=req.body.apellido;
+//   var email=req.body.email;
+//   var telefono=req.body.telefono;
+//   var mensaje=req.body.mensaje;
+// }
 
 module.exports = router;
